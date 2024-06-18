1b6839619246ce643c752b5ee2290b17e99fa5a4
Wojciech Koszela <wojkos9@gmail.com>
2024-06-18 08:00:01 +0200

    Cangjie reference in presentation card

--- src/common/KanjiCard.tsx
+++ src/common/KanjiCard.tsx
@@ -3,7 +3,7 @@ import jlpt from '../jlpt'
 import { themeNeutral, themes } from '../theme'
 import { getMeaning } from '../QuizScreen/QuizScreen'
 import { LangContext } from '../Utils'
-import groups from '../assets/groups.json'
+import cangjie from '../cangjie'
 
 function getLevel(k: string) {
   const lvls: Level[] = ["N5", "N4", "N3", "N2", "N1"]
@@ -41,28 +41,24 @@ function KanjiCompound({ compound }: {compound: Compound}) {
 
 export default function KanjiCard({ kanji, onlyMeta }: { kanji: Kanji, onlyMeta?: boolean }) {
   const lang = useContext(LangContext)
-  const group = useMemo(() =>
-    Object.entries(groups).find(([g, k]) => g != "x" && k.includes(kanji.char))
-      ?.[0] as keyof typeof groups | null
-  , [kanji])
 
-  const sims = useMemo(() =>
-    group ? groups[group]?.split("")?.filter(k => k != kanji.char) : []
-  , [group])
-  return (
-    <div className='px-4'>
-      <div className='text-xl m-1'>{getMeaning(kanji, lang).join(", ")}</div>
-      { !onlyMeta &&
-      <div className='text-8xl text-center font-[KanjiChart] bg-accent'>{kanji.char}</div>
-      }
-      <div className="my-2">
-        <div>{kanji.on.join(", ")}</div>
-        <div>{kanji.kun.join(", ")}</div>
+    return (
+      <div className='px-4'>
+        <div className='text-xl m-1'>{getMeaning(kanji, lang).join(", ")}</div>
+        { !onlyMeta &&
+        <div className='text-8xl text-center font-[KanjiChart] bg-accent'>{kanji.char}</div>
+        }
+        <div className="mt-2">
+          <div>{kanji.on.join(", ")}</div>
+          <div>{kanji.kun.join(", ")}</div>
+        </div>
+        <div className='text-xs my-1'>
+          { cangjie[kanji.char][0].toUpperCase() }
+        </div>
+        <div className='border-2 rounded p-1 border-highlight'>
+          <div className='text-xs font-bold'>Compounds</div>
+          {kanji.compound.map(c => <KanjiCompound compound={c}/>)}
+        </div>
       </div>
-      <div className='border-2 rounded p-1 border-highlight'>
-        <div className='text-xs font-bold'>Compounds</div>
-        {kanji.compound.map(c => <KanjiCompound compound={c}/>)}
-      </div>
-    </div>
-  )
+    )
   }
