62143b1be83f0cd308bc2d05cbd9ededb4c18f75
Wojciech Koszela <wojkos9@gmail.com>
2025-08-29 21:17:54 +0200

    Add PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA({
+    includeAssets: ['favicon.svg'],
+    manifest: {
+      name: 'JLPT Study',
+      short_name: 'JLPTStudy',
+      description: 'JLPT Study',
+      theme_color: '#ffffff',
+      icons: [
+        {
+          src: 'pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
+    }
+  })],
 })
